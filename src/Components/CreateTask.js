108315263f18a81867fd48c1ea373359384875ff
Kuba Sadowy <kubasadowy2@gmail.com>
2023-02-08 19:55:01 +0100

    update Component CreateTask

--- src/Components/CreateTask.js
+++ src/Components/CreateTask.js
@@ -2,14 +2,16 @@ import { useState } from "react"
 
 export const CreateTask = ({ handleCreateNewTask }) => {
     const [newTask, setNewTask] = useState({
-        taskName: '',
-        taskDate: '',
+        taskName: [],
+        taskDate: [],
     });
     const handleSubmit = (e) => {
         e.preventDefault()
         if (!newTask) {
             return;
         }
+        handleCreateNewTask(newTask);
+        setNewTask([])
     }
 
 
@@ -17,7 +19,7 @@ export const CreateTask = ({ handleCreateNewTask }) => {
         <form onSubmit={handleSubmit}>
             <label>
                 Create New Task
-                <input type="text" value={newTask.taskName} onChange={(e) => setNewTask.taskName(e.target.value)}></input>
+                <input type="text" value={newTask.taskName} onChange={(e) => setNewTask(e.target.value)}></input>
                 <input type="date" value={newTask.taskDate} onChange={(e) => setNewTask.taskDate(e.target.value)}></input>
             </label>
             <button>Add new task </button>
