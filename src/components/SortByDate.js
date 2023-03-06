677e7219fb55d49ef328201eec05256aa259ccb7
Kuba Sadowy <kubasadowy2@gmail.com>
2023-03-06 18:23:56 +0100

    update

--- src/components/SortByDate.js
+++ src/components/SortByDate.js
@@ -1,5 +1,4 @@
 import { todoData } from "../constants/INITIAL_ITEMS";
-import { ToDoListItem } from "./ToDoListItem";
 
 export default function SortByDate() {
     const dataOfInitialItemsDate = todoData.map(({ date }) => ({ date }));
