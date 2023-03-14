dcd28c6931352b1aa55c0459566b4ab2f5993144
DDobrowolski <damian.dobrowolski99@gmail.com>
2023-03-14 18:54:26 +0100

    Fixed sorting logic

--- src/components/SortByDate.js
+++ src/components/SortByDate.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 
 
-export default function SortByDate({ todo, setTodo }) {
+export default function SortByDate({ setTodo }) {
     const [sortingType, setSortingType] = useState("DESC");
     const sort = () => {
-        console.log(todo)
-
-        todo.sort((a, b) => b.date > a.date ? (sortingType === "ASC" ? 1 : -1) : -1);
-
         setSortingType(sortingType === "DESC" ? "ASC" : "DESC");
-        setTodo([...todo]);
-
+        setTodo(prev => [...prev].sort((a, b) => b.date > a.date ? (sortingType === "ASC" ? 1 : -1) : -1));
     };
     return (
         <button type="submit" onClick={sort} className="sort-date-btn">
